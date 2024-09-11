8b1859cfe8f80912970fec87c5c90c5597ea2f27
sareesh <sareesh@gmail.com>
2024-09-11 19:16:07 +0530

    commit by sareesh

---
+++ StreamApi/src/com/nit/sari/StreamDemo.java
@@ -0,0 +1,18 @@
+package com.nit.sari;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamDemo {
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("sareesh", "varun", "advik", "mallesh");
+
+
+		List<String> result = names.stream().filter(s->s.startsWith("s"))
+				.collect(Collectors.toList());
+
+		System.out.println(result);
+	}
+
+}
